820e07edf36195e1571f7014e47a877ec127ff99
Isaac Otom <isaacotom@gmail.com>
2023-10-07 14:34:12 +0300

    Update cryptopricepredictionwebapp.py

--- cryptopricepredictionwebapp.py
+++ cryptopricepredictionwebapp.py
@@ -13,7 +13,7 @@ today = date.datetime.today().strftime('%Y-%m-%d')
 
 # Streamlit Web App
 # Give the web app a title
-st.title('Crypto Close Price Prediction Web App')
+st.title('Crypto Close Price Prediction Using GRU Model')
 
 coins = ('BTC-USD', 'USDT-USD', 'LTC-USD', 'XRP-USD', 'ETH-USD')
 
